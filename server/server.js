e3fb6b491fd078c6e789d2ab935daa9fe7d40dda
Your Name <rahuljogadiya007@gmail.com>
2025-06-23 10:24:11 +0530

    server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["https://zestoria.netlify.app"],
+    origin: ["https://snacksy.netlify.app"],
     credentials: true,
   })
 );
